7d521c10ef65072643f10011b4176b3936df1b3f
unilock <unilock@fennet.rentals>
2022-12-17 22:59:45 -0500

    only fire advancement event when *entire* advancement is complete i.e. the advancement for exploring every nether biome is now sent only once, when all biomes have been explored, rather than every time a new biome was explored (thus fulfilling only *part* of the advancement)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/ooo/foooooooooooo/yep/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/ooo/foooooooooooo/yep/mixin/PlayerAdvancementTrackerMixin.java
@@ -1,6 +1,7 @@
 package ooo.foooooooooooo.yep.mixin;
 
 import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementProgress;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.server.network.ServerPlayerEntity;
 import ooo.foooooooooooo.yep.events.AdvancementCallback;
@@ -13,10 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerAdvancementTracker.class)
 public class PlayerAdvancementTrackerMixin {
     @Shadow
+    public AdvancementProgress getProgress(Advancement advancement) {
+        return null;
+    }
+
+    @Shadow
     private ServerPlayerEntity owner;
 
     @Inject(method = "grantCriterion", at = @At(value = "TAIL"))
     public void onAdvancement(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
-        AdvancementCallback.EVENT.invoker().getAdvancement(this.owner, advancement);
+        AdvancementProgress advancementProgress = this.getProgress(advancement);
+        if (advancementProgress.isDone()) {
+            AdvancementCallback.EVENT.invoker().getAdvancement(this.owner, advancement);
+        }
     }
 }
