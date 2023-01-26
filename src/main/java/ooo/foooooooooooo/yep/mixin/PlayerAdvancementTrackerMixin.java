32137af185d350821bb62678913de74e15dc1970
unilock <unilock@fennet.rentals>
2023-01-25 22:10:26 -0500

    only fire callback event on advancements announced to chat
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/ooo/foooooooooooo/yep/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/ooo/foooooooooooo/yep/mixin/PlayerAdvancementTrackerMixin.java
@@ -1,7 +1,6 @@
 package ooo.foooooooooooo.yep.mixin;
 
 import net.minecraft.advancement.Advancement;
-import net.minecraft.advancement.AdvancementProgress;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.server.network.ServerPlayerEntity;
 import ooo.foooooooooooo.yep.events.AdvancementCallback;
@@ -14,18 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerAdvancementTracker.class)
 public class PlayerAdvancementTrackerMixin {
     @Shadow
-    public AdvancementProgress getProgress(Advancement advancement) {
-        return null;
-    }
-
-    @Shadow
     private ServerPlayerEntity owner;
 
-    @Inject(method = "grantCriterion", at = @At(value = "TAIL"))
+    @Inject(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"))
     public void onAdvancement(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
-        AdvancementProgress advancementProgress = this.getProgress(advancement);
-        if (advancementProgress.isDone()) {
-            AdvancementCallback.EVENT.invoker().getAdvancement(this.owner, advancement);
-        }
+        AdvancementCallback.EVENT.invoker().getAdvancement(this.owner, advancement);
     }
 }
